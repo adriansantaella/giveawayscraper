ed4ca2996cbb8297e9143d152ba0fb79cfddb699
Adrian Santaella <asantaella@cgasolutions.com>
2024-09-17 13:47:23 -0400

    Messin with api fetch

--- frontend/index.js
+++ frontend/index.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', (e) => {
 
 	let handleSubmit = async (numOfPages) => {
 		try {
-			const response = await fetch(`http://localhost:8080/scrape-data?numpages=${numOfPages}`);
+			const response = await fetch(`https://giveawayscraper.vercel.app/api/scraper?numpages=${numOfPages}`);
 			if (!response.ok) {
 				throw new Error('Response came back with errors. Try again later...');
 			}
